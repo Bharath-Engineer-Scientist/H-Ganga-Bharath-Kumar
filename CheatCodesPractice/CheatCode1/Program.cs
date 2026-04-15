9a9ab865618b2042fb6738b59f2b4b6d6dd1a3c8
Your Name <your@email.com>
2026-04-15 11:40:36 +0530

    Add project files.

---
+++ CheatCodesPractice/CheatCode1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*int score = 6;
+if (score = 6)
+{
+    Console.WriteLine("Boundary!");
+}*/
+
+int score = 6;
+if (score == 6)
+{
+    Console.WriteLine("Boundary!");
+}
+else
+{
+    Console.WriteLine("Not a boundary");
+}
